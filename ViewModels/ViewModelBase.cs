e8b23d796086fb25866daf8200180be5baad48f4
Michail2013 <ngorpro@gmail.com>
2021-08-26 16:10:07 +0300

    Designed ViewModelBase

--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using ReactiveUI;
 
 namespace Cov19Test.ViewModels
 {
-    public class ViewModelBase : ReactiveObject
+    // internal abstract class ViewModelBase : ReactiveObject
+    // {
+
+    // }
+    internal abstract class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if(Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
     }
 }
