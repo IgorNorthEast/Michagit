b1f365d3167948c5141b118894d4f9c5cab37eb3
Michail2013 <ngorpro@gmail.com>
2021-08-26 15:52:56 +0300

    First startup commit for Cov19 project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cov19Test.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "StrartUp";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Cov19Test.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
