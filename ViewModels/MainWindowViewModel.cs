dca10aa02a9b2486b9fba5951ee4805a919ad454
Michail2013 <ngorpro@gmail.com>
2021-08-27 17:03:46 +0300

    CommandBase class released with test command

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using System.Net.Mime;
+using System.Windows.Input;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Cov19Test.Infrastructure.Commands.Base;
 
 namespace Cov19Test.ViewModels
 {
@@ -15,5 +18,40 @@ namespace Cov19Test.ViewModels
             set => Set(ref _Title, value);
         }
         #endregion
+
+        #region Window status
+        /// <summary> Window status </summary>
+        private string _Status = "Ready!";
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+        #endregion
+
+        #region Command
+
+        public ICommand CloseApplicationCommand { get; }
+
+        private void OnCloseApplicationCommandExecuted()
+        {
+            if (Avalonia.Application.Current.ApplicationLifetime is Avalonia.Controls.ApplicationLifetimes.IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.Shutdown();
+            }
+        }
+        private bool CanCloseApplicationCommandExecute() => true;
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+
+            #region Commands
+
+            CloseApplicationCommand = new CommandBase(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
+        }
     }
 }
