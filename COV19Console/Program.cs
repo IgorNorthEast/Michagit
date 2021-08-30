ab7be2c200b73380f1a8c62e3655f34cfe530797
Michail2013 <ngorpro@gmail.com>
2021-08-30 15:35:25 +0300

    Launch.json for ConsoleApp configurated

--- COV19Console/Program.cs
+++ COV19Console/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 
 namespace COV19Console
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string data_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_time_series/time_series_19-covid-Confirmed_archived_0325.csv";
+            
+            var client = new HttpClient();
+            var response = client.GetAsync(data_url).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+            Console.ReadLine();
+            Console.WriteLine("s");
         }
     }
 }
