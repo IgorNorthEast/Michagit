b08bd25a84439a53f7e61a245c186a5e8153748c
Michail2013 <ngorpro@gmail.com>
2021-08-31 11:40:03 +0300

    MainWindowViewModel update

--- COV19WPF/ViewModels/MainWindowViewModel.cs
+++ COV19WPF/ViewModels/MainWindowViewModel.cs
@@ -4,11 +4,25 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using COV19WPF.Infrastructure.Commands.Base;
+using COV19WPF.Models;
 
 namespace COV19WPF.ViewModels
 {
     internal class MainWindowViewModel : ViewModelBase
     {
+        #region TestDataPoint
+
+        /// <summary> Test data collection for graphics visualization </summary>
+        private IEnumerable<DataPoint> _TestDataPoints;
+        public IEnumerable<DataPoint> TestDataPoints
+        {
+            get => _TestDataPoints;
+            set => Set(ref _TestDataPoints, value);
+        }
+            
+        #endregion
+        
+
         #region Window Title
         /// <summary> Window title </summary>
         private string _Title = "COVID-19 Analisys";
@@ -52,6 +66,17 @@ namespace COV19WPF.ViewModels
             CloseApplicationCommand = new CommandBase(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
 
             #endregion
+
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+            for (var x = 0d; x < 30; x += 0.1)
+            {
+                const double to_rad = Math.PI / 360;
+                var y = Math.Sin(x + to_rad);
+
+                data_points.Add(new DataPoint { XValue = x, YValue = y});
+            }
+
+            TestDataPoints = data_points;
         }
     }
 }
